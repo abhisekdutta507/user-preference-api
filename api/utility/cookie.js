b5ade1de219d63d0ea391af5ea9dd03138e7b0cf
Abhisek Dutta <abhisek.dutta.507@gmail.com>
2023-08-23 22:23:36 +0530

    experimental changes

--- api/utility/cookie.js
+++ api/utility/cookie.js
@@ -1,4 +1,4 @@
 export const getCookie = (data = {}) => {
   const maxAge = 5 * 60 * 1000; // 5 minutes
-  return ['Authorization', data.token, { domain: 'user-preference-ui.netlify.app', sameSite: 'None', path: '/', secure: true, httpOnly: true, maxAge }];
+  return ['Authorization', data.token, { domain: '.netlify.app', sameSite: 'None', path: '/', secure: true, httpOnly: true, maxAge }];
 };
